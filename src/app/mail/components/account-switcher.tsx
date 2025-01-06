b134def7ef777bada5982d7bdf2e868bc4305e52
Martin-Lianzhan-Gao <martingaoorglz@gmail.com>
2025-01-06 17:56:14 +1000

    built account-switcher ui

---
+++ src/app/mail/components/account-switcher.tsx
@@ -0,0 +1,72 @@
+"use client";
+import React from "react";
+import { api } from "~/trpc/react";
+import { useLocalStorage } from "usehooks-ts";
+import { Select, SelectContent, SelectTrigger, SelectValue, SelectItem } from "~/components/ui/select";
+import { cn } from "~/lib/utils";
+import { Plus } from "lucide-react";
+
+
+type Props = {
+    isCollapsed: boolean
+}
+
+const AccountSwitcher = ({ isCollapsed }: Props) => { 
+
+    // always get selected account id 
+    const [accountId, setAccountId] = useLocalStorage('accountId', '');
+
+    const { data } = api.account.getAccounts.useQuery();
+    // check data existence
+    if (!data) return null;
+    
+    return (
+        <Select defaultValue={accountId} onValueChange={setAccountId}> 
+            <SelectTrigger
+                className={cn(
+                "flex w-full flex-1 items-center gap-2 [&>span]:line-clamp-1 [&>span]:flex [&>span]:w-full [&>span]:items-center [&>span]:gap-1 [&>span]:truncate [&_svg]:h-4 [&_svg]:w-4 [&_svg]:shrink-0",
+                isCollapsed &&
+                "flex h-9 w-9 shrink-0 items-center justify-center p-0 [&>span]:w-auto [&>svg]:hidden"
+                )}
+                aria-label="Select a account"
+            >
+                <SelectValue placeholder="Select an account">
+
+                    {/* When isCollapsed is false, apply hidden class */}
+                    <span className={cn({ 'hidden': !isCollapsed })}>
+                        { /* Minimal display, just show the first letter of email address */}
+                        {data.find((account) => account.id === accountId)?.emailAddress[0]}
+                    </span>
+
+                    {/* When isCollapsed is true, apply hidden class */}
+                    <span className={cn({ 'hidden': isCollapsed }, 'ml-2')}>
+                        { /* Full display, show full email address */}
+                        {data.find((account) => account.id === accountId)?.emailAddress}
+                    </span>
+
+                </SelectValue>
+            </SelectTrigger>
+            { /* For unfold display */}
+            <SelectContent>
+                {data.map((account) => {
+                    return (
+                        // set all accounts of the user (authed) as options
+                        // value will be used as accountId
+                        <SelectItem key={account.id} value={account.id}>{account.emailAddress}</SelectItem>
+                    )
+                })}
+                <div className="flex relative hover:bg-gray-50 w-full cursor-pointer items-center rounded-sm py-1.5 pl-2 pr-8 text-sm outline-none focus:bg-accent focus:text-accent-foreground data-[disabled]:pointer-events-none data-[disabled]:opacity-50"
+                    onClick={async () => { 
+                        
+                    }}
+                >
+                    <Plus className="size-4 mr-1"></Plus>
+                    Add account
+                </div>
+            </SelectContent>
+        </Select>
+    )
+
+}
+
+export default AccountSwitcher;
---
+++ src/app/mail/mail.tsx
@@ -0,0 +1,99 @@
+'use client'
+import { TabsContent, TabsTrigger } from "~/components/ui/tabs";
+import React from "react";
+import { ResizablePanel, ResizableHandle, ResizablePanelGroup } from "~/components/ui/resizable";
+import { Separator } from "~/components/ui/separator";
+import { Tabs, TabsList } from "~/components/ui/tabs";
+import { TooltipProvider } from "~/components/ui/tooltip";
+import { cn } from "~/lib/utils";
+import AccountSwitcher from "./components/account-switcher";
+
+// define props structure
+type Props = {
+    defaultLayout: number[] | undefined
+    navCollapseSize: number
+    defaultCollapsed: boolean
+}
+
+const Mail = ({ defaultLayout = [20, 32, 48], navCollapseSize, defaultCollapsed }: Props) => { 
+    // side bar collapse state
+    const [isCollapsed, setCollapsed] = React.useState(defaultCollapsed);
+
+    return (
+         // define the delay time of tooltip pop-up
+         <TooltipProvider delayDuration={0}>
+             <ResizablePanelGroup direction="horizontal" onLayout={(sizes: number[]) => {
+                 // notice new sizes of panel group while change
+                 console.log(sizes)
+            }} className="items-stretch h-full min-h-screen">
+                { /* Side Bar Area */ }
+                <ResizablePanel
+                    defaultSize={defaultLayout[0]}
+                    collapsedSize={navCollapseSize}
+                    collapsible={true}
+                    minSize={15}
+                    maxSize={40}
+                    // side bar collapse event
+                    onCollapse={() => setCollapsed(true)}
+                    // side bar expand & resize event
+                    onResize={() => {
+                        setCollapsed(false);
+                     }}
+                    // apply transition to all transitionable elements, smooth both start and end
+                    // className applied only when side bar collapsed
+                    className={cn(isCollapsed &&'min-w-[50px] transition-all duration-300 ease-in-out')}
+                >
+                    {/* Side Bar Sub Elements */ }
+                    <div className="flex flex-col h-full flex-1">
+                        {/* Account Switcher Area */}
+                        <div className={cn("flex h-[52px] items-center justify-between", isCollapsed ? 'h-[52px]' : 'px-2')}>
+                            {/* Account Switcher */}
+                            <AccountSwitcher isCollapsed={isCollapsed}></AccountSwitcher>
+                        </div>
+                        <Separator />
+                        {/* Email Category Side Bar */}
+                        Email Categories Side Bar
+                        <div className="flex-1"></div>
+                        {/* Main Content */}
+                        ASK AI
+                    </div>
+                </ResizablePanel>
+                <ResizableHandle withHandle />
+                <ResizablePanel
+                    defaultSize={defaultLayout[1]}
+                    minSize={30}
+                >
+                    <Tabs defaultValue="Inbox"> 
+                        <div className="flex items-center px-4 py-2">
+                            <h1 className="text-xl font-bold">Inbox</h1>
+                            <TabsList className="ml-auto">
+                                <TabsTrigger value="Inbox" className="text-xinc-600 dark:text-zinc-200">
+                                    Inbox
+                                </TabsTrigger>
+                                <TabsTrigger value="Done" className="text-xinc-600 dark:text-zinc-200">
+                                    Done
+                                </TabsTrigger>
+                            </TabsList>
+                        </div>
+                        <Separator />
+                        {/* Search Bar */}
+                        Search Bar
+                        <TabsContent value="Inbox">
+                            Inbox Emails
+                        </TabsContent>
+                        <TabsContent value="Done">
+                            Done Emails
+                        </TabsContent>
+                    </Tabs>
+                </ResizablePanel>
+                <ResizableHandle withHandle />
+                <ResizablePanel defaultSize={defaultLayout[2]} minSize={30}>
+                    {/* Threads Display */}
+                    Threads View
+                </ResizablePanel>
+             </ResizablePanelGroup>
+        </TooltipProvider>
+     )
+}
+
+export default Mail;
