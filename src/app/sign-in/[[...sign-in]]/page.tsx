7b19907dbae74f2c52fec358b469d77504e7b210
Martin-Lianzhan-Gao <martingaoorglz@gmail.com>
2025-03-25 19:09:54 +1000

    Feat: Beautify sign-in page and sign-up page

--- src/app/sign-in/[[...sign-in]]/page.tsx
+++ src/app/sign-in/[[...sign-in]]/page.tsx
@@ -1,7 +1,30 @@
+'use client'
 import { SignIn } from '@clerk/nextjs'
 
 import React from 'react'
+import { cn } from '~/lib/utils';
 
 export default function Page() {
-    return <SignIn />
+
+    const [isHovering, setIsHovering] = React.useState(false);
+    
+    const handleMouseEnter = () => {
+        setIsHovering(true);
+    };
+    
+    const handleMouseLeave = () => {
+        setIsHovering(false);
+    };
+
+    return (
+        <div className='absolute inset-0 flex items-center justify-center h-screen w-screen'>
+            <img className='h-full w-full object-cover object-center' src='/background.jpg'></img>
+            <div className={cn('absolute w-fit h-fit', isHovering ? "animate-none" : "animate-breath-float")} 
+                onMouseEnter={handleMouseEnter}
+                onMouseLeave={handleMouseLeave}>
+                <SignIn />
+            </div>
+        </div>
+
+    )
 }
--- src/app/sign-up/[[...sign-up]]/page.tsx
+++ src/app/sign-up/[[...sign-up]]/page.tsx
@@ -1,7 +1,29 @@
-import { SignUp } from '@clerk/nextjs' 
+'use client'
+import { SignUp } from '@clerk/nextjs'
 
 import React from 'react'
+import { cn } from '~/lib/utils';
 
 export default function Page() {
-    return <SignUp />
+    const [isHovering, setIsHovering] = React.useState(false);
+
+    const handleMouseEnter = () => {
+        setIsHovering(true);
+    };
+
+    const handleMouseLeave = () => {
+        setIsHovering(false);
+    };
+
+    return (
+        <div className='absolute h-screen w-screen inset-0 flex items-center justify-center'>
+            <img className='h-full w-full object-cover' src='/background.jpg'></img>
+            <div className={cn('absolute w-fit h-fit', isHovering ? "animate-none" : "animate-breath-float")} 
+                onMouseEnter={handleMouseEnter}
+                onMouseLeave={handleMouseLeave}
+            >
+                <SignUp />
+            </div>
+        </div>
+    )
 }
